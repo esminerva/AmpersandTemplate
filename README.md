67abed851d096e193648dfc56811e5d41b012fc8
joy01804 <joy.y.chang@utexas.edu>
2015-04-11 19:53:17 -0500

    Initial Commit
    Some Ampersand stuffs

---
+++ README.md
@@ -0,0 +1,4 @@
+## How to run it
+
+1. download/install [node.js](http://nodejs.org/)
+2. install dependencies: `npm install`
---
+++ server.js
@@ -0,0 +1,57 @@
+/* global console */
+var path = require('path');
+var express = require('express');
+var helmet = require('helmet');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var compress = require('compression');
+var config = require('getconfig');
+var semiStatic = require('semi-static');
+var serveStatic = require('serve-static');
+var stylizer = require('stylizer');
+var templatizer = require('templatizer');
+var app = express();
+
+// a little helper for fixing paths for various environments
+var fixPath = function (pathString) {
+    return path.resolve(path.normalize(pathString));
+};
+
+
+// -----------------
+// Configure express
+// -----------------
+app.use(compress());
+app.use(serveStatic(fixPath('public')));
+
+// we only want to expose tests in dev
+if (config.isDev) {
+    app.use(serveStatic(fixPath('test/assets')));
+    app.use(serveStatic(fixPath('test/spacemonkey')));
+}
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// in order to test this with spacemonkey we need frames
+if (!config.isDev) {
+    app.use(helmet.xframe());
+}
+app.use(helmet.xssFilter());
+app.use(helmet.nosniff());
+
+app.set('view engine', 'jade');
+
+
+// -----------------
+// Set our client config cookie
+// -----------------
+app.use(function (req, res, next) {
+    res.cookie('config', JSON.stringify(config.client));
+    next();
+});
+
+// listen for incoming http requests on the port as specified in our config
+app.listen(config.http.port);
+console.log('Ampersand Template is running at: http://localhost:' + config.http.port + ' Yep. That\'s pretty awesome.');
