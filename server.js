6d137b4f02e10e3567aefcd7348b1e6b4041ff11
joy01804 <joy.y.chang@utexas.edu>
2015-04-11 20:29:09 -0500

    Server Side rendering
    Fix server side first so server can start

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var compress = require('compression');
 var config = require('getconfig');
-var semiStatic = require('semi-static');
-var serveStatic = require('serve-static');
-var stylizer = require('stylizer');
-var templatizer = require('templatizer');
 var app = express();
 
 // a little helper for fixing paths for various environments
@@ -21,15 +17,6 @@ var fixPath = function (pathString) {
 // -----------------
 // Configure express
 // -----------------
-app.use(compress());
-app.use(serveStatic(fixPath('public')));
-
-// we only want to expose tests in dev
-if (config.isDev) {
-    app.use(serveStatic(fixPath('test/assets')));
-    app.use(serveStatic(fixPath('test/spacemonkey')));
-}
-
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -50,6 +37,10 @@ app.set('view engine', 'jade');
 app.use(function (req, res, next) {
     res.cookie('config', JSON.stringify(config.client));
     next();
+});
+
+app.get('*', function(req, res) {
+    res.render('ampersand');
 });
 
 // listen for incoming http requests on the port as specified in our config
